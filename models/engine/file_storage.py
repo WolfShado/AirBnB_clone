bf867186ad979958efc42779588c4d65838f0e13
WolfShado <chamess.souib@gmail.com>
2023-10-16 03:43:46 -0700

    hot fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage"""
+"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
 
 
 class FileStorage:
-    """This class manages storage in JSON format"""
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Retrieves a Dictionary of Stored Models"""
+        """Return the dictionary __objects."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Inserts new object into storage dictionary"""
-        objname = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(objname, obj.id)] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
-        object_dict = {}
-        for key, obj in FileStorage.__objects.items():
-            object_dict[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(object_dict, file)
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                object_dict = json.load(file)
-                for key, val in object_dict.items():
-                    class_name, obj_id = key.split('.')
-                    _class = eval(class_name)
-                    obj = _class(**val)
-                    self.new(obj)
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
